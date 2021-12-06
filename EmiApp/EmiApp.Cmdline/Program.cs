46536a4a3a17c6d89831d543f85f9991d44166f3
Scurlly Rodrigues <92797431+scurlly-rodrigues-kilowott@users.noreply.github.com>
2021-12-06 18:56:41 +0530

    added regions

--- EmiApp/EmiApp.Cmdline/Program.cs
+++ EmiApp/EmiApp.Cmdline/Program.cs
@@ -9,31 +9,34 @@ namespace EmiApp.Cmdline
             var emiRequest = new Request() { Principal = 100000, InterestRateInPercentage = 12, LoanDurationInYearCount = 2 };
             ICalculator emiCalculator = new Calculator();
 
-            
+            #region
             var emiMonthly = emiCalculator.CalculateMonthlyEmi(emiRequest);
         
             if (string.IsNullOrEmpty(emiMonthly.ErrorMessage))
                 Console.WriteLine($"Monthly EMI is {emiMonthly.EmiPayment}");
             else 
                 Console.WriteLine(emiMonthly.ErrorMessage);
+            #endregion
 
 
             // fill other regions below...
-
+            #region
             var emiDaily = emiCalculator.CalculateDailyEmi(emiRequest);
 
             if (string.IsNullOrEmpty(emiMonthly.ErrorMessage))
                 Console.WriteLine($"Daily EMI is {emiDaily.EmiPayment}");
             else
                 Console.WriteLine(emiMonthly.ErrorMessage);
+            #endregion
 
+            #region
             var emiContinous = emiCalculator.CalculateContinousEmi(emiRequest);
 
             if (string.IsNullOrEmpty(emiMonthly.ErrorMessage))
                 Console.WriteLine($"Continous EMI is {emiContinous.EmiPayment}");
             else
                 Console.WriteLine(emiMonthly.ErrorMessage);
-
+            #endregion
         }
     }
 }
