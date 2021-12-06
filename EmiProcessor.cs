407c402fee8eec5879e29b1ce5d64679d59ddb6a
Scurlly Rodrigues <92797431+scurlly-rodrigues-kilowott@users.noreply.github.com>
2021-12-06 11:13:16 +0530

    Add project files.

---
+++ EmiProcessor.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EMICalculator
+{
+    internal class EmiProcessor
+    {
+        internal decimal P { private get; set; }
+
+        internal decimal i { private get; set; }
+
+        internal int n { private get; set; }
+
+        internal int t { private get; set; }
+
+        public decimal CalculateEmi()
+        {
+            t = t * n;
+            i = 1 + (i / n);
+           
+            decimal e = P * ((decimal) (Math.Pow((double)i,(double)t))) / 24;
+
+            decimal emi = Math.Round(e, 2);
+
+            return emi;
+
+            
+
+        }
+       
+    }
+
+}
